9b28f8cec4b02856b0d0959f87a0c8cb6359656e
Dan Reuman <reuman@ku.edu>
2021-03-04 15:12:54 -0600

    code for averaging off diagonal entries of a matrix, should have committed this a while back

--- Code/AvgOffDiags.R
+++ Code/AvgOffDiags.R
@@ -1,12 +1,21 @@
-#Takes an array of dimensions n by n by m and averages the off-diagonal vectors.
+#Takes an array of dimensions n by n by m and averages the off-diagonal vectors. Prior to averaging,
+#if the second argument is not NA, elements of the first argument are rescaled in a way that uses
+#the second argument.
 #
 #Args
 #A          The array
+#resc       Vector of length n, for a certain kind of rescaling prior to averaging. Default
+#             NA means don't perform any rescaling.
 #
 #Output
 #A vector of length m
 #
-avgoffdiags<-function(A)
+#Details
+#Picture that A is Sww, or some component of that, and we are rescaling by geometric
+#means of variances of time series. That's what this is for. The argument resc should
+#just be the variances of the time series.
+#
+avgoffdiags<-function(A,resc=NA)
 {
   #error catching
   if (dim(A)[1]!=dim(A)[2])
@@ -17,6 +26,20 @@ avgoffdiags<-function(A)
   {
     stop("Error in avgoffdiags: A must have all finite entries")
   }
+  if (!is.na(resc[1]) && (length(resc)!=dim(A)[1] || !all(is.finite(resc))))
+  {
+    stop("Error in avgoffdiags: bad value for resc")
+  }
+  
+  #get ready for the normalization, if desired
+  if (!is.na(resc[1]))
+  {
+    rescsqrt<-sqrt(resc)
+    rescmat<-outer(X=rescsqrt,Y=rescsqrt,FUN="*")
+  }else
+  {
+    rescmat<-matrix(1,nrow(A),ncol(A))  
+  }
   
   n<-dim(A)[1]
   m<-dim(A)[3]
@@ -24,7 +47,7 @@ avgoffdiags<-function(A)
   {
     h<-A[,,counter]
     diag(h)<-NA
-    A[,,counter]<-h
+    A[,,counter]<-h/rescmat
   }
   
   res<-apply(FUN=mean,X=A,MARGIN=3,na.rm=TRUE)
--- Code/tests_AvgOffDiags.R
+++ Code/tests_AvgOffDiags.R
@@ -3,6 +3,8 @@ context("AvgOffDiags.R")
 source("AvgOffDiags.R")
 
 test_that("test it",{
+  #***for the default case
+  
   #test error catching
   A<-array(1,c(2,3,4))
   testthat::expect_error(avgoffdiags(A),"Error in avgoffdiags: A must have dimensions n by n by m")
@@ -26,4 +28,31 @@ test_that("test it",{
   h<-A[,,3]
   diag(h)<-NA
   testthat::expect_equal(res[3],mean(h,na.rm=TRUE))
+  
+  #***for the case where resc is provided
+  
+  #test error catching
+  resc<-c(1,2,3)
+  testthat::expect_error(avgoffdiags(A,resc),"Error in avgoffdiags: bad value for resc")
+  resc<-c(1,2,3,4,NA)
+  testthat::expect_error(avgoffdiags(A,resc),"Error in avgoffdiags: bad value for resc")
+  
+  #test a case that should equal the default case
+  resc<-rep(1,5)
+  testthat::expect_equal(avgoffdiags(A),avgoffdiags(A,resc))
+  
+  #now test a case which actually uses a non-trivial resc
+  resc<-1:5
+  res1<-avgoffdiags(A,resc)
+  rescmat<-outer(sqrt(resc),sqrt(resc),FUN="*")
+  rescmat<-array(rescmat,c(5,5,3))
+  A<-A/rescmat
+  for (counter in 1:3)
+  {
+    h<-A[,,counter]
+    diag(h)<-NA
+    A[,,counter]<-h
+  }
+  res2<-apply(FUN=mean,X=A,MARGIN=3,na.rm=TRUE)
+  testthat::expect_equal(res1,res2)
 })
