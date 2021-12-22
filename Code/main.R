e302d3a5ac723134e665a8a262825ffe91c425e9
Dan Reuman <reuman@ku.edu>
2021-12-22 09:04:59 -0600

    starting to change the overall control flow to drop the attempt to study the plankton

--- Code/main.R
+++ Code/main.R
@@ -7,7 +7,8 @@ rm(list=ls())
 graphics.off()
 
 #Develop the theoretical case studies
-source("Theory_CaseStudyA.R")
+source("Theory_CaseStudyAB.R")
+source("Theory_CaseStudyC.R")
 
 #Clean the kelp data
 source("Kelp_AnalysisStep_CleanData_SuperBasic.R")
@@ -24,12 +25,3 @@ source("Kelp_AnalysisStep_LinearModelSelection.R")
 #Do the spectra decomposition analysis for kelp
 source("Kelp_AnalysisStep_SpectralDecomp.R")
 
-#Clean the plankton data
-source("Plankton_AnalysisStep_CleanData_Basic.R")
-
-#Plot plankton time series for visual inspection
-source("Plankton_AnalysisStep_PlotTimeSeries.R")
-
-#Do linear models with pci and related data to help demo the need for specific 
-#lags - this also performs and saves the final level of data cleaning
-source("Plankton_AnalysisStep_LinearModels.R")
